686bc1d87031c0d641f05f5b34075beb3e796f54
Mate Mong <mongmate811@gmail.com>
2023-01-25 18:32:37 +0100

    game works now

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,69 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+const colorLookup = ["red", "orange", "yellow", "green", "blue"];
+
 function App() {
-  const [towers, setTowers] = useState([3, 0, 0]);
+  const [towers, setTowers] = useState([[5, 4, 3, 2, 1], [], []]);
+  const [selectedTowerIndex, setSelectedTowerIndex] = useState<
+    number | undefined
+  >();
+
+  function handleClickedTower(clickedTowerIndex: number) {
+    if (selectedTowerIndex !== undefined) {
+      const selectedTower = towers[selectedTowerIndex];
+      const clickedTower = towers[clickedTowerIndex];
+
+      if (
+        selectedTower[selectedTower.length - 1] >
+        clickedTower[clickedTower.length - 1]
+      ) {
+        setSelectedTowerIndex(undefined);
+        return;
+      }
+      if (selectedTower.length === 0) { setSelectedTowerIndex(undefined) 
+        return;}
+      const newTowers = [...towers];
+     
+      const poppedDisc = newTowers[selectedTowerIndex].pop();
+      newTowers[clickedTowerIndex].push(poppedDisc!);
+      setTowers(newTowers);
+      setSelectedTowerIndex(undefined);
+
+      if (clickedTower.length === 5 && clickedTowerIndex === 2) {
+        alert("Victory royale");
+      }
+    } else {
+      setSelectedTowerIndex(clickedTowerIndex);
+    }
+  }
 
   return (
     <div className="App">
       <div className="towers">
-        {towers.map((towerHeight, index) => (
-          <div key={index} className="tower">
-
+        {towers.map((discs, towerIndex) => (
+          <div
+            key={towerIndex}
+            className={
+              "tower " + (selectedTowerIndex === towerIndex ? "selected" : "")
+            }
+            onClick={() => handleClickedTower(towerIndex)}
+          >
             <div className="line"></div>
             <div className="discs">
-              {[...new Array(towerHeight)].map((disc, idx) => (
-                <div
-                  className="disc"
-                  style={{
-                    width: `${idx * 25 + 25}px`,
-                  }}
-                ></div>
-              ))}
+              {discs.map((discNumber) => {
+                const color = colorLookup[discNumber-1];
+                return (
+                  <div
+                    key={discNumber}
+                    className="disc"
+                    style={{
+                      width: `${discNumber * 25 + 25}px`,
+                      backgroundColor: `${color}`,
+                    }}
+                  ></div>
+                );
+              })}
             </div>
           </div>
         ))}
